c0b05ad4d25f99fb42bb391c60e502763d57d5e4
LEFA MOLOKWE <fluidair2010@gmail.com>
2025-10-06 00:47:55 +0200

    Update reports.py

--- frontend/reports.py
+++ frontend/reports.py
@@ -3,7 +3,7 @@ import requests
 import json
 from api_client import APIClient
 
-API_BASE_URL = "http://127.0.0.1:8000"
+API_BASE_URL = "https://erb-backend.onrender.com"
 
 
 def fetch_reports(token: str):
